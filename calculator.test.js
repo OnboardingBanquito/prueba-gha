512d146e2b33754aa0221457ecfc26c5eab7cf95
Jorge <jorgeivanreyesvalencia92@gmail.com>
2024-01-12 10:54:04 -0500

    refactor

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const math = require('./calculator');
 test('prueba suma', () => {
   expect(math.suma(1,2)).toBe(3);
 })
+
+test('prueba multiplicacion', () => {
+  expect(math.multiplicacion(2,2)).toBe(4);
+})
